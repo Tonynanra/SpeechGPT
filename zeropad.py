93cd1c98c8b21a0dccdeecc481b8b14ae9b1716d
TacoL <34144367+TacoL@users.noreply.github.com>
2024-04-15 15:41:32 -0400

    some progress

--- zeropad.py
+++ zeropad.py
@@ -1,19 +1,19 @@
 import numpy as np
 import scipy.signal as signal
 
-def zeropad(audio_signal, sample_rate)
+def zeropad(audio_signal, sample_rate):
 
     #find target number of samples to get to 30 s
     tgt_length = 30 * sample_rate
     
     #add zeroes if its too short
-    if size(audio_signal) < tgt_length
-        while size(audio_signal) < tgt_length
+    if size(audio_signal) < tgt_length:
+        while size(audio_signal) < tgt_length:
             audio_signal.append(0)
             
     #truncate if its too long
-    else
-        while size(audio_signal) > tgt_length
+    else:
+        while size(audio_signal) > tgt_length:
             audio_signal.pop()
     
     return audio_signal
